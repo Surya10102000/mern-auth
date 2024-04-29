0aa8c49cf84c5268b4036a903df09436db9dafbd
Surya Prakash Mallah <sfrost488@gmail.com>
2024-04-29 19:22:41 +0530

    Created the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from  "./pages/Signin"
+import Signup from  "./pages/Signup"
+import About from  "./pages/About"
+import Profile from  "./pages/Profile"
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-700">Hello world!</h1>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
