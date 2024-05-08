43a31955d722f34537fee2367ecc279b3c70186a
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-08 20:03:17 +0530

    complete sign up page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,31 +1,61 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import Loading from "../components/Loading";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [error , setError] = useState(false);
+  const [loading , setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      setLoading(true)
+      setError(false)
+      e.preventDefault();
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/sign-up",
+        formData
+      );
+      console.log(response);
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }
+  };
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-        <form className="flex flex-col gap-4 ">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
           <input
             type="text"
             placeholder="Username"
             id="username"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="Email"
             id="email"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
           />
           <input
             type="password"
             placeholder="Password"
             id="password"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
           />
-          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            Sign up
+          <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            {loading ? <Loading/> : "Sign Up"}
           </button>
         </form>
         <div className="flex gap-2 mt-5">
@@ -34,6 +64,7 @@ const Signup = () => {
             <span className="text-blue-500">Sign in</span>
           </Link>
         </div>
+        <p className="text-red-700 mt-5">{error && "something went wrong!!"}</p>
       </div>
     </>
   );
--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import connectDB from "./db/db.js"
 import authRoutes from "./routes/authRoute.js"
 import { errorHandler } from './middlewares/error.js'
+import cors from 'cors'
 
 connectDB() 
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
